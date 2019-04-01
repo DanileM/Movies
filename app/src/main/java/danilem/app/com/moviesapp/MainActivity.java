0f77e602a1fbc3d9099eb683e65b552bed721424
Данила Мартынов <danbkamartynov@gmail.com>
2019-04-01 17:44:06 +0300

    Add viewModel

--- app/src/main/java/danilem/app/com/moviesapp/MainActivity.java
+++ app/src/main/java/danilem/app/com/moviesapp/MainActivity.java
@@ -1,26 +1,23 @@
 package danilem.app.com.moviesapp;
 
 import android.app.Activity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.List;
-
 import danilem.app.com.moviesapp.Module.Example;
-import danilem.app.com.moviesapp.Module.Value;
-import danilem.app.com.moviesapp.REST.JSONPlaceHolderApi;
-import danilem.app.com.moviesapp.REST.NetworkService;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MoviesApp";
 
@@ -49,36 +46,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.rc_movies);
+        MoviesViewModel model = ViewModelProviders.of(this).get(MoviesViewModel.class);
 
+        recyclerView = findViewById(R.id.rc_movies);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setHasFixedSize(true);
 
-        JSONPlaceHolderApi service = NetworkService.getJSONApi();
-        Call <Example> call = service.getValues();
-        call.enqueue(new Callback<Example>() {
+        model.getMovies().observe(this, new Observer<Example>() {
             @Override
-            public void onResponse(Call<Example> call, Response<Example> response) {
-                Example data = response.body();
-
-                if(response.isSuccessful()) {
-                    if(data != null) {
-                        MoviesAdapter moviesAdapter = new MoviesAdapter(MainActivity.this, data);
-                        recyclerView.setAdapter(moviesAdapter);
-
-                        Log.i(TAG, "Response call");
-                    }else{
-                        Log.i(TAG, "Data is null");
-                    }
-                }else{
-                    Log.i(TAG, "Response does not successful");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Example> call, Throwable t) {
-                Log.i(TAG, "Failure call");
+            public void onChanged(@Nullable Example example) {
+                MoviesAdapter adapter = new MoviesAdapter(MainActivity.this, example);
+                recyclerView.setAdapter(adapter);
             }
         });
     }
