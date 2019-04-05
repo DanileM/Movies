f709f5b70247d79d46120a129b3dc77226301588
Данила Мартынов <danbkamartynov@gmail.com>
2019-04-05 15:40:13 +0300

    trying to do filter

---
+++ app/src/main/java/danilem/app/com/moviesapp/Header.java
@@ -0,0 +1,14 @@
+package danilem.app.com.moviesapp;
+
+public class Header extends ListItem {
+
+    private String header;
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+}
---
+++ app/src/main/java/danilem/app/com/moviesapp/Rows.java
@@ -0,0 +1,27 @@
+package danilem.app.com.moviesapp;
+
+public class Rows extends ListItem {
+
+    String row;
+    private boolean isSelected;
+
+    public Rows(String row) {
+        this.row = row;
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+    public void setRow(String row) {
+        this.row = row;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
+}
